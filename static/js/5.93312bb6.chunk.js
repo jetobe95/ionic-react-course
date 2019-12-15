1753e77b2df6a5077319909828197e243548569d
Jeffrey Torres <jtorres@technisupport.com>
2019-12-15 14:07:18 -0500

    Updates

---
+++ static/js/5.93312bb6.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpionic-react"]=this["webpackJsonpionic-react"]||[]).push([[5],{184:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return o}));var i=e(25),o=(e(3),function(){var n=window;n.addEventListener("statusTap",(function(){Object(i.g)((function(){var t=n.innerWidth,e=n.innerHeight,o=document.elementFromPoint(t/2,e/2);if(o){var c=o.closest("ion-content");c&&c.componentOnReady().then((function(){Object(i.m)((function(){return c.scrollToTop(300)}))}))}}))}))})}}]);
+//# sourceMappingURL=5.93312bb6.chunk.js.map
